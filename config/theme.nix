2976eed5ea3efe3865f7a7784ba16b0bede63dcc
Ethan Lew <elew@galois.com>
2023-09-17 11:25:04 -0700

    make a custom statusline with git information

--- config/theme.nix
+++ config/theme.nix
@@ -5,6 +5,25 @@
 
   plugins.lightline = {
     enable = true;
+
+    # redo the status line
+    active = {
+      left = [
+        ["mode" "paste"]
+        ["readonly" "filename" "modified"]
+      ];
+
+      right = [ 
+        [ "lineinfo" ]
+        [ "percent" ]
+        [ "fileformat" "fileencoding" "filetype" "charvaluehex" ]
+        [ "git" ]
+      ];
+    };
+
+    component = {
+      git = "%{fugitive#statusline()}";
+    };
   };
   
   colorschemes.catppuccin = {

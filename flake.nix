a3afeeef44348100b9198e6d378fcb7df1782e90
Ethan Lew <elew@galois.com>
2024-05-04 14:51:57 -0700

    print nvim nix to json target

--- flake.nix
+++ flake.nix
@@ -6,19 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    nixvim,
-    flake-utils,
-    ...
-  } @ inputs: let
+  outputs = { nixpkgs, nixvim, flake-utils, ... } @ inputs: let
     importedConfig = import ./config;
     importedDockerConfig = import ./docker_config;
-    optionsConfig = import ./keymaps.nix { lib=nixpkgs.lib; };
+    optionsConfig = import ./keymaps.nix { lib = nixpkgs.lib; };
   in
     flake-utils.lib.eachDefaultSystem (system: let
       nixvimLib = nixvim.lib.${system};
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs { inherit system; };
       nixvim' = nixvim.legacyPackages.${system};
       nvim = nixvim'.makeNixvimWithModule {
         inherit pkgs;
@@ -27,7 +22,6 @@
       nvim-docker = nixvim'.makeNixvimWithModule {
         inherit pkgs;
         module = optionsConfig // (importedDockerConfig { inherit pkgs system; });
-
       };
       dockerImage = pkgs.dockerTools.buildImage {
         name = "elew-vim";
@@ -37,18 +31,19 @@
           Cmd = [ "${nvim-docker}/bin/nvim" ];
         };
       };
+      nvimJSON = pkgs.writeText "nvim-config.json" (builtins.toJSON nvim);
     in {
       checks = {
-        # Run `nix flake check .` to verify that your config is not broken
         default = nixvimLib.check.mkTestDerivationFromNvim {
           inherit nvim;
           name = "elew nixvim configuration";
         };
       };
       packages = {
-        # Lets you run `nix run .` to start nixvim
         inherit dockerImage;
         default = nvim;
+        nvimJSON = nvimJSON;
       };
     });
 }
+
